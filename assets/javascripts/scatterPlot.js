9947f5ccdaa66590b72af5bb8b73058df46b4a93
Leo Salat <leo.salat@gmail.com>
2017-05-05 18:43:44 -0700

    add scaling

--- assets/javascripts/scatterPlot.js
+++ assets/javascripts/scatterPlot.js
@@ -3,6 +3,15 @@ import * as d3 from 'd3'
 const scatterPlot = (dataset) => {
   const w = 500;
   const h = 500;
+  const padding = 60;
+
+  const xScale = d3.scaleLinear()
+                   .domain([0, d3.max(dataset, (d) => d[0])])
+                   .range([padding, w - padding]);
+
+  const yScale = d3.scaleLinear()
+                   .domain([0, d3.max(dataset, (d) => d[1])])
+                   .range([h - padding, padding]);
 
   const svg = d3.select("body")
                 .append("svg")
@@ -14,8 +23,8 @@ const scatterPlot = (dataset) => {
      .data(dataset)
      .enter()
      .append("circle")
-     .attr("cx", d => d[0])
-     .attr("cy", d => h - d[1])
+     .attr("cx", d => xScale(d[0]))
+     .attr("cy", d => h - yScale(d[1]))
      .attr("r", 5)
      .attr("fill", "black")
 
@@ -23,8 +32,8 @@ const scatterPlot = (dataset) => {
      .data(dataset)
      .enter()
      .append("text")
-     .attr("x", d => d[0] + 10)
-     .attr("y", d => h - d[1])
+     .attr("x", d => xScale(d[0] + 10))
+     .attr("y", d => h - yScale(d[1]))
      .text(d => d.join(", "))
 
 }
